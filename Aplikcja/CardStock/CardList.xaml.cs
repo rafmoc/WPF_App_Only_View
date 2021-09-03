79dfbe473b9979d173e4476c8d4c39a6620dc0f5
rafmoc <olejniczak00@gmail.com>
2021-09-03 13:18:43 +0200

    Fixing format

---
+++ Aplikcja/CardStock/CardList.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aplikcja.CardStock
+{
+    public partial class CardList : Page
+    {
+        private MainWindow mainWindow;
+        public CardList()
+        {
+            InitializeComponent();
+
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(MainWindow))
+                {
+                    mainWindow = (MainWindow)window;
+                }
+            }
+        }
+
+        private void CardStockCardNew(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Main.Content = new CardStock.New();
+        }
+        private void CardStockCardImport(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Main.Content = new CardStock.Import();
+        }
+        private void CardStockCardExport(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Main.Content = new CardStock.Export();
+        }
+        private void CardStockCardTests(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Main.Content = new CardStock.CardTests();
+        }
+        private void CardStockCardOrders(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Main.Content = new CardStock.CardOrders();
+        }
+        private void CardStockCardList(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Main.Content = new CardStock.CardList();
+        }
+    }
+}
---
+++ Aplikcja/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aplikcja
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Main.Content = new MainPage();
+        }
+        public void HomePage(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new MainPage();
+        }
+        public void CiList(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Configuration.CiList();
+        }
+        public void DpEpMatching(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Configuration.DpEpMatch();
+        }
+        public void CiNew(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Configuration.CInew();
+        }
+        public void PaList(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PA.PaList();
+        }
+        public void PaCiMatching(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new PA.PaCiMatch();
+        }
+
+        //----------------------------------------------------------
+
+        private void CardStockCardNew(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CardStock.New();
+        }
+        private void CardStockCardImport(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CardStock.Import();
+        }
+        private void CardStockCardExport(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CardStock.Export();
+        }
+        private void CardStockCardTests(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CardStock.CardTests();
+        }
+        public void CardStockCardOrders(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CardStock.CardOrders();
+        }
+        public void CardStockCardList(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new CardStock.CardList();
+        }
+    }
+}
+
+
